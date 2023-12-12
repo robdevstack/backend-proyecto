ba06337bd4ac04804e8f7ec82b28d926d77f8848
roberto vera <robertodevstack@gmail.com>
2023-12-12 19:02:04 -0300

    db

---
+++ marketplace.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE marketplace;
+CREATE TABLE posts (
+  id SERIAL PRIMARY KEY,
+  titulo VARCHAR(1000),
+  img TEXT,
+  descripcion TEXT,
+  precio VARCHAR(25)
+);
+CREATE TABLE usuarios (
+	id SERIAL PRIMARY KEY,
+	email VARCHAR(255),
+	password TEXT
+);
+INSERT INTO posts (id, titulo, img, descripcion, precio) VALUES (1, 'auto rojo clasico', 'https://docs.gimp.org/2.8/es/images/tutorials/quickie-jpeg-100.jpg', 'Auto clasico de lujo bien cuidado por años', 3500000  );
